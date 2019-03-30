b38a62671fedba1376773091ff52e43916f6ce5d
Ben Reeves <breeves997@gmail.com>
2019-03-30 07:50:23 -0600

    Pull upcoming events into calendar

--- client/src/App.js
+++ client/src/App.js
@@ -20,23 +20,46 @@ const meetups = [
 
 const apiRoot = 'http://localhost:8080'
 
-const localizer = Calendar.momentLocalizer(moment) 
+const localizer = Calendar.momentLocalizer(moment)
+
+const EventComponent = (event) => {
+  return (
+    <span><strong>{event.title} </strong></span>
+  )
+}
+
+class MyEvent extends Component {
+  constructor(event) {
+    console.log(event)
+    super(event)
+    this.state = { event: event.event }
+  }
+
+  handleClick = () => {
+    console.log('clicked')
+    window.open(this.state.event.resource.link);
+  }
+
+  render = () => {
+    return (
+      <div onClick={(e) => this.handleClick(e)}>
+        <span><strong>{this.state.event.title} </strong></span>
+      </div>
+
+    )
+  }
+}
 
 class YYCCalendar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      events: [
-        {
-          start: new Date(),
-          end: new Date(moment().add(1, "days")),
-          title: "Some title"
-        }
-      ]
+      events: []
     };
 
     this.setState = this.setState.bind(this);
+    this.groups = this.groups.bind(this);
 
 
   }
@@ -45,49 +68,98 @@ class YYCCalendar extends Component {
     return old.concat(toAdd);
   }
 
-  componentDidMount = () => {
-    this.setState({
-      events: [
-        {
-          start: new Date(),
-          end: new Date(moment().add(1, "days")),
-          title: "Some title 2"
-        }
-      ]
-    })
-    axios.get(apiRoot + '/events/PyData-Calgary')
-      .then(response =>  {
-        console.log(response);
+  groups = () => {
+    return axios.get(apiRoot + '/groups')
+      .then(response => {
+        const data = response.data;
+        return data.map(x => x.id);
+      });
+  }
+
+  getEventsFor = (group) => {
+    return axios.get(apiRoot + '/events/' + group)
+      .then(response => {
         const data = response.data;
         const events = [];
-        if(!data.length) return;
-        for(let i = 0; i < data.length; i++) {
+        if (!data.length) return [];
+        for (let i = 0; i < data.length; i++) {
           const event = data[i];
           const start = moment(event.local_date + " " + event.local_time);
-          const end = start.add(event.duration, 'ms');
-
+          const end = moment(start).add(event.duration, 'ms');
           events.push({
-            start: start,
-            end: end,
+            start: new Date(start),
+            end: new Date(end),
             title: event.name,
-            resource: event.venue.name
+            resource: {
+              venue: event.venue,
+              group: group,
+              link: event.link
+
+            }
+            ,
 
           })
+
+          return events
         }
-        const newEvents = this.extendEvents(this.state.events, events);
-        this.setState({events: newEvents})
+
       })
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      events: []
+    })
+    this.groups().then(groupIds => {
+      let requests = groupIds.map(this.getEventsFor)
+      Promise.all(requests)
+        .then(eventsArrays => {
+          let allEvents = eventsArrays.reduce((a, b) => a.concat(b))
+          this.setState({ events: allEvents });
+        })
+    })
+
+    // axios.get(apiRoot + '/events/PyData-Calgary')
+    //   .then(response =>  {
+    //     console.log(response);
+    //     const data = response.data;
+    //     const events = [];
+    //     if(!data.length) return;
+    //     for(let i = 0; i < data.length; i++) {
+    //       const event = data[i];
+    //       const start = moment(event.local_date + " " + event.local_time);
+    //       const end = start.add(event.duration, 'ms');
+
+    //       events.push({
+    //         start: start,
+    //         end: end,
+    //         title: event.name,
+    //         resource: event.venue.name
+
+    //       })
+    //     }
+    //     const newEvents = this.extendEvents(this.state.events, events);
+    //     this.setState({events: newEvents})
+    //   })
   }
 
   render() {
     return (
+      <div>
+
+        <h2>Upcoming Events</h2>
+        <div className="mt-2">
+
         <Calendar
           localizer={localizer}
           defaultDate={new Date()}
           defaultView="month"
           events={this.state.events}
+          components={{ event: MyEvent }}
           style={{ height: "100vh" }}
         />
+        </div>
+      </div>
     );
   }
 }
@@ -101,35 +173,49 @@ function About() {
   return <h2>About</h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
+function Groups() {
+  return <h2>Groups</h2>;
 }
 
 function AppRouter() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/calendar">YYCCalendar</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">YYC Data Collective</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/calendar">YYCCalendar</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about/">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/groups/">Groups</Link>
+              </li>
+            </ul>
+          </div>
+
         </nav>
 
-        <Route path="/" exact component={Index} />
-        <Route path="/calendar" component={YYCCalendar} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
+        <div className="container-fluid">
+          <div className="mt-3">
+
+
+
+            <Route path="/" exact component={Index} />
+            <Route path="/calendar" component={YYCCalendar} />
+            <Route path="/about/" component={About} />
+            <Route path="/groups/" component={Groups} />
+          </div>
+        </div>
       </div>
     </Router>
   );
