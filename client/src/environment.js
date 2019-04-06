05906633f3bc315508503ec8a12532aea0124118
Ben Reeves <breeves@viewpointgroup.ca>
2019-04-06 11:47:15 -0600

    should have included api as base path

--- client/src/environment.js
+++ client/src/environment.js
@@ -3,7 +3,7 @@
 //     : 'http://localhost:8080/api';
 let apiRoot = window.location.hostname === 'localhost' 
     ? 'http://localhost:3001/api'
-     : '';
+     : 'api';
 
 export default {
     apiRoot: apiRoot
